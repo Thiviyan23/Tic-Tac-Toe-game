c9f60cabbd9e7868a6bcafb0c5c7653b73ac6854
Thiviyan23 <it25102938@my.sliit.lk>
2025-10-05 23:27:04 +0530

    game

--- Tic-Tac-Toe.c
+++ Tic-Tac-Toe.c
@@ -1,108 +1,244 @@
-#include<stdio.h>
-     
-char arr[10]= {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
- 
-void creatingboard() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-    printf("\n\nTic-Tac-Toe\n\n");
-    printf("player 1 (X) -- player 2 (O)\n");
+char **board;                                      // the game board using pointer to 2D array
+int N;                                             // board size
+char players[3] = {'X','O','Z'};                   // for 3 player game mode
 
-    printf("     |     |     \n");
-    printf(" %c   | %c   | %c \n", arr[1],arr[2],arr[3]);
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-    printf(" %c   | %c   | %c \n", arr[4],arr[5],arr[6]);
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-    printf(" %c   | %c   | %c \n", arr[7],arr[8],arr[9]);
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-}
-void marking(int choice, char mark) {
-
- if(choice==1 && arr[1]=='1')       //choosing the boxes
-      arr[1] = mark;
-    else if(choice==2 && arr[2]=='2')
-    arr[2] = mark;
-else if(choice==3 && arr[3]=='3')
-    arr[3] = mark;
-else if(choice==4 && arr[4]=='4')
-    arr[4] = mark;
-else if(choice==5 && arr[5]=='5')
-    arr[5] = mark;
-else if(choice==6 && arr[6]=='6')
-    arr[6] = mark;
-else if(choice==7 && arr[7]=='7')
-    arr[7] = mark;
-else if(choice==8 && arr[8]=='8')
-    arr[8] = mark;
-else if(choice==9 && arr[9]=='9')
-    arr[9] = mark;
-else
-    printf("invalid choice\n");
-
-}
-
-
-int checkwin() {
-
-//horizontal matching
-if(arr[1]==arr[2] && arr[2]==arr[3])
-return 1;
-else if(arr[4]==arr[5] && arr[5]==arr[6])
-return 1;
-else if(arr[7]==arr[8] && arr[8]==arr[9])
-return 1;
-//
-//vertical matching
-else if(arr[1]==arr[4] && arr[4]==arr[7])
-return 1;
-else if(arr[2]==arr[5] && arr[5]==arr[8])
-return 1;
-else if(arr[3]==arr[6] && arr[6]==arr[9])
-return 1;
-//
-//diagonal matching
-else if(arr[1]==arr[5] && arr[5]==arr[9])
-return 1;
-else if(arr[3]==arr[5] && arr[5]==arr[9])
-return 1;
-//
-//draw condition
-else if(arr[1]!='1' && arr[2]!='2' && arr[2]!='2'&& arr[3]!='3'&& arr[4]!='4'&& arr[5]!='5'&& arr[6]!='6'&& arr[7]!='7'&& arr[8]!='8'&& arr[9]!='9')
-return 0;
-
-return -1;
-}
-
-int main(){
- int choice, player=1,i;
-    char mark;
-    do{
-        creatingboard();
-        player = (player %2)? 1: 2;
-        
-        printf("player %d: Select a number between 1 to 9: ",player);
-        scanf("%d", &choice);
-
-        mark = (player==1)? 'X': 'O';
-        marking(choice, mark);
-
-        i =checkwin();
-        player++;
-
-
+void makeBoard() {                                 // make the board using malloc
+    board = (char **)malloc(N * sizeof(char *));   // allocate rows
+    for (int i = 0; i < N; i++) {
+        board[i] = (char *)malloc(N * sizeof(char));  // allocate each row
     }
-     while(i==-1);
 
-     creatingboard();
-     if(i==1)
-        printf("Player %d won the game",--player);
-     else
-        printf("<=======Draw=======>");
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            board[i][j] = ' ';                       // for space
+}
+
+
+void showBoard() {                                   // show board on screen with row and column numbers
+    printf("\n   ");  
+    for (int j = 0; j < N; j++) {
+        printf(" %d  ", j);                          // column numbers
+    }
+    printf("\n");
+
+    for (int i = 0; i < N; i++) {
+        printf(" %d ", i);                           // row numbers
+        for (int j = 0; j < N; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < N - 1) printf("|");              // seperating with line
+        }
+        printf("\n");
+
+        if (i < N - 1) {
+            printf("   ");                          // align lines 
+            for (int k = 0; k < N; k++) {
+                printf("---");
+                if (k < N - 1) printf("+");
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+int win(char s) {                                   // check win
+    int i,j;
+    for (i = 0; i < N; i++) {                            //  check all row
+        int check=1;
+        for (j = 0; j < N; j++) 
+        if (board[i][j]!=s) check=0;
+        if (check) 
+        return 1;
+    }
+
+    for (j = 0; j < N; j++) {                            // check all column
+        int check=1;
+        for (i = 0; i < N; i++) 
+        if (board[i][j]!=s) check=0;
+        if (check) 
+        return 1;
+    }
+
+    int check=1;                                      // check diagonal left to right
+    for (i = 0; i < N; i++) 
+    if (board[i][i]!=s) check=0;
+    if (check) 
+    return 1;
+    check=1;                                          //  check diagonal right to left
+    for (i = 0; i < N; i++) 
+    if (board[i][N-1-i]!=s) check=0;
+    if (check) 
+    return 1;
+
+    return 0;                                     // if no win
+}
+
+int draw() {                                      // check draw
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            if (board[i][j]==' ') 
+            return 0;                             // still empty box
+                                         
+    return 1;                                     // board is full
+}
+
+void computerMove() {                                 // computer makes move
+    int r,c; // row and column
+    do {
+        r = rand()%N;                             // pick random row
+        c = rand()%N;                             // pick random column
+    } while (board[r][c] != ' ');                 // repeat if cell is not empt
+    board[r][c] = 'O';                            // computer symbol 'O'                  
+    printf("Computer moves to %d %d\n",r,c);
+}
+
+// Output
+int main() {
+    printf("TIC TAC TOE GAME\n");
+
+    printf("Enter board size N (3 <= N <= 10): ");   // get board size from user
+    scanf("%d",&N);
+
+    makeBoard();                                     // create board
+
+    int mode;                                        // choose game mode
+    printf("Select Game Mode: \n");
+    printf("1. Two players (User vs User)\n");
+    printf("2. User vs computer\n");
+    printf("3. Three players\n");
+    printf("Enter choice (1-3): ");
+    scanf("%d",&mode);
+
+    srand(time(0));                                 // for computer random
+
+    FILE *f;                                        // open file for saving moves
+    f = fopen("logfile.txt","w");
+
+    int turn=0;                                     // 0=X,1=O,2=Z
+    int row,col;
+
+    while (1) {
+        showBoard();                                // display board at each turn
+        char now;
+
+        if (mode==1) {                              // user vs user game mode
+            now = (turn==0)?'X':'O';                // pick current player symbol
+            printf("Player %c enter row and col: ",now);
+            scanf("%d %d",&row,&col);               // get move from user
+
+            if (row<0 || row>=N || col<0 || col>=N || board[row][col]!=' ') {           // check if move is valid
+                printf("wrong move\n");
+                continue; 
+            }
+
+            board[row][col]=now;                      // make the move
+            fprintf(f,"Player %c moves (row and col): %d %d\n",now,row,col);    //saves in file
+
+            if (win(now)) {                           //check win and save it
+                showBoard();
+                printf("Player %c Is The Winner\n",now); 
+                fprintf(f,"Player %c Is The Winner \n",now);
+                break; 
+            }
+
+            if (draw()) {                             // check draw and save it
+                showBoard(); 
+                printf("Draw\n"); 
+                fprintf(f,"Draw\n"); 
+                break; 
+            }
+
+            turn = 1-turn;                           // swap turns
+
+        } else if (mode==2) {                        // user vs computer game mode
+            
+            printf("You are X Enter row and col: ");   // X is user
+            scanf("%d %d",&row,&col);
+
+            if (row<0 || row>=N  || col<0 || col>=N || board[row][col]!=' ') {  // check if move is valid
+                printf("wrong move\n"); 
+                continue; 
+            }
+            board[row][col]='X';                                               // make the move
+            fprintf(f,"Player X moves (row and col): %d %d\n",row,col);
+
+            if (win('X')) {                                                   //check win and save it
+                showBoard(); 
+                printf("Player X Is The Winner\n"); 
+                fprintf(f,"X Is The Winner\n"); 
+                break; 
+            }
+            if (draw()) {                                                     // check draw and save it
+                showBoard(); 
+                printf("Draw\n"); 
+                fprintf(f,"Draw\n"); 
+                break; 
+            }
+
+            
+            computerMove();                                                       // computer turn
+
+            for (int i = 0; i < N; i++)                                                // make the computer move
+            for (int j = 0; j < N; j++) 
+            if (board[i][j]=='O') 
+            {row=i;col=j;} 
+            fprintf(f,"Computer 'O' moves (row and col): %d %d\n",row,col);
+
+            if (win('O')) {                             // check if computer wins
+                showBoard(); 
+                printf("COMPUTER Is The Winner\n"); 
+                fprintf(f,"Computer Wins\n"); 
+                break; 
+            }
+            if (draw()) {                              // check if computer draw
+                showBoard(); 
+                printf("Draw\n"); 
+                fprintf(f,"Draw\n"); 
+                break; 
+            }
+
+        } else if (mode==3) {                     // Three players game mode
+            now = players[turn];                  // pick player's symbol
+
+            printf("Player %c enter row and col: ",now);                                 // get move
+            scanf("%d %d",&row,&col);
+
+            if (row<0 || row>=N || col<0 || col>=N || board[row][col]!=' ') {            // check if move is valid
+                printf("wrong move\n"); 
+                continue; 
+            }
+
+            board[row][col]=now;                                                          // make move
+            fprintf(f,"Player %c moves (row and col):  %d %d\n",now,row,col);
+
+            if (win(now)) {                                                               //check win and save it
+                showBoard(); 
+                printf("Player %c Is The Winner\n",now); 
+                fprintf(f,"Player %c Is The Winner\n",now); 
+                break; 
+            }
+            if (draw()) {                                                                  //check draw and save it
+                showBoard(); 
+                printf("Draw\n"); 
+                fprintf(f,"Draw\n"); 
+                break; 
+            }
+
+            turn = (turn+1)%3;                     // move to the next player
+        }
+    }
+
+    fclose(f);                                    // close logfile
+    printf("\nGame saved in logfile.txt\n");
+
+for (int i = 0; i < N; i++) {                     // free allocated memory
+    free(board[i]);
+}
+free(board);
+
     return 0;
 }
-
-
-
-
